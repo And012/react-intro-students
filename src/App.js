4d07647db366e00a76f4e63551cdbada513fc723
Andranik <andtsatur1296@gmail.com>
2022-02-14 18:28:43 +0400

    add functional and class component example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+// import ClassComp from "./component/class";
+// import FuncComponent from "./component/function";
+
 function App() {
   return <h1>We starting learn React(սովորենք Րեակտ գրադարանը)</h1>;
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <FuncComponent />
+//       <ClassComp />
+//     </div>
+//   );
+// }
---
+++ src/component/function.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FuncComponent() {
+  return <h1>this is functional component</h1>;
+}
