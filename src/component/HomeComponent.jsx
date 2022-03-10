082b614d57b0cbe76af12b0354cd11e4a6710c96
Andranik <andtsatur1296@gmail.com>
2022-03-10 16:39:58 +0400

    added some subroutes

--- src/component/HomeComponent.jsx
+++ src/component/HomeComponent.jsx
@@ -1,3 +1,13 @@
+import {useNavigate} from "react-router-dom";
+import {Routes, Route} from 'react-router-dom'
 export default function Home() {
-    return <div>home</div>
+    const navigate = useNavigate()
+
+    return <div>
+        <h1>this is home</h1>
+        <button onClick={() => navigate('/')}>go to main page</button>
+        <Routes>
+            <Route path={'firstFloor'} element={<div>first floor</div>}/>
+        </Routes>
+    </div>
 }
